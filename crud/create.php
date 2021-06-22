d73776431d6762533ac33f42729b172e7c9efdaa
Marcio Douglas <dmarcio998@gmail.com>
2021-06-22 20:52:06 -0300

    gjuhd,ghuy,

---
+++ crud/create.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "p0ikfg5byl9da62n";
+$password = "l99vczrt1bkayb2r";
+$dbname    = "jznz94d4b9izdizu";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO white (aluno, disciplina, nota1, nota2) 
+  VALUES ('Marcio ', 'espanhol', '10','10')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "aluno cadastrado";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
---
+++ crud/read.php
@@ -0,0 +1,44 @@
+<?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>aluno</th><th>disciplina</th><th>nota1</th><th>nota2</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+  function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+  }
+
+  function current() {
+    return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+  }
+
+  function beginChildren() {
+    echo "<tr>";
+  }
+
+  function endChildren() {
+    echo "</tr>" . "\n";
+  }
+}
+
+$servername = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "p0ikfg5byl9da62n";
+$password = "l99vczrt1bkayb2r";
+$dbname    = "jznz94d4b9izdizu";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT * FROM white");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>
